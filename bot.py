eb6d5787ae170d18dbb069d86a91458cff78ac11
ROO73D <73015074+ROO73D@users.noreply.github.com>
2024-03-23 17:44:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -426,20 +426,26 @@ def handle_all_messages(message):
               url = re.sub(r'https:\/\/[a-zA-Z1-90.]+\/file\/','https://gdflix.lol/file/',i.strip())
               url1 = re.sub(r'https:\/\/[a-zA-Z1-90.]+\/file\/','https://new.gdflix.cfd/file/',i.strip())
               headers = {
-                  'authority': 'nerd.gdflix.ink',
+                  'authority': 'new.gdflix.cfd',
                   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                   'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
                   'cache-control': 'max-age=0',
-                  # 'cookie': 'PHPSESSID=7b163f62e6fa3b100922534210867fb6',
-                  'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+                  # 'cookie': 'cf_chl_3=35c155dddfd27f1; cf_clearance=8LC5U.Ejlpu9V4Sr8ssWe6RlBnJUcek4VKNKnjI1jvU-1711195979-1.0.1.1-WQ22Qi6megEupgkldfUKOR.YTqnKczaQfEdMEdiDpdvUDEmRHwZH3Fbmlyyq4DIzuer3rWvBA8JX_0u81uDq1A; PHPSESSID=19885a6ca8dc997879ceed05797553c3',
+                  'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+                  'sec-ch-ua-arch': '"x86"',
+                  'sec-ch-ua-bitness': '"64"',
+                  'sec-ch-ua-full-version': '"122.0.6261.131"',
+                  'sec-ch-ua-full-version-list': '"Chromium";v="122.0.6261.131", "Not(A:Brand";v="24.0.0.0", "Google Chrome";v="122.0.6261.131"',
                   'sec-ch-ua-mobile': '?0',
+                  'sec-ch-ua-model': '""',
                   'sec-ch-ua-platform': '"Windows"',
+                  'sec-ch-ua-platform-version': '"15.0.0"',
                   'sec-fetch-dest': 'document',
                   'sec-fetch-mode': 'navigate',
                   'sec-fetch-site': 'none',
                   'sec-fetch-user': '?1',
                   'upgrade-insecure-requests': '1',
-                  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+                  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
               }
               html = requests.get(url1.strip(),headers=headers)
               soup = BeautifulSoup(html.text,'lxml')
