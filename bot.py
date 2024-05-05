08018e7dd7577f931cc5e6f4bfb641b8c68ce24c
ROO73D <73015074+ROO73D@users.noreply.github.com>
2024-05-05 14:05:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -490,7 +490,7 @@ def handle_all_messages(message):
                   "url":f"{url1}",
                   "maxTimeout": 60000
                 }
-                response = requests.post('https://flaresolverrr45-cd010c722fa5.herokuapp.com/v1', headers={'Content-Type': 'application/json'}, json=post_body)
+                response = requests.post('https://flaresolverr-059375af3dd1.herokuapp.com/v1', headers={'Content-Type': 'application/json'}, json=post_body)
                 fsdata = json.loads(response.text)
                 soup = BeautifulSoup(fsdata['solution']['response'],'lxml')
                 if soup.title.text != 'GDFlix | GDFlix':
